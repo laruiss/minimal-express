14360212242a75680a314cd6d06b48d05991a1d7
Stanislas Ormières <stan@stormier.ninja>
2023-09-17 19:25:43 +0200

    feat: :sparkles: add route to get version

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/version', (req, res) => {
+  res.json({ version: '0.0.1' })
 })
 
 app.listen(port, () => {
